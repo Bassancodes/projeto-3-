cae96529a45599de751781178f04c2f047f09b2e
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-04-26 23:23:50 -0300

    Update funcoes.h
    funcoes. h ja comentada

--- funcoes.h
+++ funcoes.h
@@ -1,21 +1,22 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
 
-struct Tarefa;
-struct ListaDeTarefas;
-int cadastrarTarefa();
-int deletarTarefa();
-int listarTarefas();
-void printMenu();
-int salvarTarefas();
-int carregarTarefas();
-void menu();
-void abrearquivo();
-void alterarTarefa();
-void filtrarPorPrioridade();
-void filtrarPorEstado();
-void filtrarPorCategoria();
-void filtrarPorPrioridadeECategoria();
-void exportarPorPrioridade();
-void exportarPorCategoria();
-void exportarPorPrioridadeECategoria();
-void printexportar();
-void exportar();
+#define MAX_CONTATOS 255 // Define o tamanho máximo da agenda de contatos.
+
+typedef struct { // Define a estrutura de dados para armazenar informações de contato.
+    char nome[50];
+    char sobrenome[50];
+    char email[100];
+    char telefone[15];
+} Contato;
+
+extern Contato agenda[MAX_CONTATOS]; // Declara um array externo de contatos com tamanho MAX_CONTATOS.
+extern int num_contatos; // Declara uma variável externa para armazenar o número de contatos atualmente na agenda.
+
+void adicionarContato(); // Protótipo da função para adicionar um novo contato.
+void listarContatos(); // Protótipo da função para listar todos os contatos.
+void deletarContato(); // Protótipo da função para deletar um contato existente.
+void salvarAgenda(); // Protótipo da função para salvar a lista de contatos em um arquivo.
+void carregarAgenda(); // Protótipo da função para carregar a lista de contatos de um arquivo.
+
+#endif
